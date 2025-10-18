aa3602d34bf94a0151706f02afbd926a104b8efd
ChrisSasias1 <qmqnptnn56@privaterelay.appleid.com>
2025-10-18 00:07:15 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
 
+function Generadorexcusas(quien, action, que, cuando) {
+  let numQuien = Math.floor(Math.random() * quien.length);
+  let numAction = Math.floor(Math.random() * action.length);
+  let numQue = Math.floor(Math.random() * que.length);
+  let numCuando = Math.floor(Math.random() * cuando.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let e = " ";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return (
+    quien[numQuien] + e + action[numAction] + e + que[numQue] + e + cuando[numCuando]
+  );
+}
+function onLoad() {
+  let excusa = document.getElementById("excusa");
+  excusa.innerHTML = Generadorexcusas(who, action, what, when);
+}
+window.onload = onLoad;
